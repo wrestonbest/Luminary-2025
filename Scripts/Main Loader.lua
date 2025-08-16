e1e584ba903a6c29f0b0e88fdc4b185aca714ab8
wrestonbest <wreston7@hotmail.com>
2025-08-16 03:39:45 +0300

    Update Main Loader.lua

--- Scripts/Main Loader.lua
+++ Scripts/Main Loader.lua
@@ -1,5 +1,5 @@
 local p = game.PlaceId
 -- build a plane
 if p == 137925884276740 then
-
+loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Build%20a%20Plane.lua'))()
 end
