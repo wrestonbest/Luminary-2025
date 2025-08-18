05ff8be8fe2b3d43e62b32614ec6ad8c24eb7062
wrestonbest <wreston7@hotmail.com>
2025-08-18 05:49:19 +0300

    Update Main Loader.lua

--- Scripts/Main Loader.lua
+++ Scripts/Main Loader.lua
@@ -3,3 +3,7 @@ local p = game.PlaceId
 if p == 137925884276740 then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Build%20a%20Plane.lua'))()
 end
+-- eat the world
+if p == 16480898254 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Eat%20the%20World.lua'))()
+end
