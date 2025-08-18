50388251d1505d7f29336d06dc56a8b8b7e19a5a
wrestonbest <wreston7@hotmail.com>
2025-08-18 18:38:51 +0300

    Update Main Loader.lua

--- Scripts/Main Loader.lua
+++ Scripts/Main Loader.lua
@@ -7,3 +7,7 @@ end
 if p == 16480898254 then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Eat%20the%20World.lua'))()
 end
+-- pure soccer
+if p == 88920112778598 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Pure%20Soccer.lua'))()
+end
