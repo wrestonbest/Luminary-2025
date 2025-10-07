f0619d3aedec0305d9886b7597d46e24b965445d
wrestonbest <wreston7@hotmail.com>
2025-10-07 03:06:24 +0300

    Update Main Loader.lua

--- Scripts/Main Loader.lua
+++ Scripts/Main Loader.lua
@@ -1,23 +1,23 @@
 local p = game.PlaceId
 -- build a plane
 if p == 137925884276740 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Build%20a%20Plane.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Build%20a%20Plane.lua'))()
 end
 -- eat the world
 if p == 16480898254 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Eat%20the%20World.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Eat%20the%20World.lua'))()
 end
 -- pure soccer
 if p == 88920112778598 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Pure%20Soccer.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Pure%20Soccer.lua'))()
 end
 -- Dont Wake the Brainrots
 if p == 118915549367482 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Dont%20Wake%20the%20Brainrots.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Dont%20Wake%20the%20Brainrots.lua'))()
 end
 -- Ninja Legends
 if p == 3956818381 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Ninja%20Legends.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Ninja%20Legends.lua'))()
 end
 
 -- Luminary X Rise Evade
@@ -27,20 +27,25 @@ end
 
 -- Break your Bones
 if p == 123821081589134 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Break%20Your%20Bones.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Break%20Your%20Bones.lua"))()
 end
 
 -- Cook Brainrot
 if p == 126224010852243 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Cook%20a%20Brainrot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Cook%20a%20Brainrot.lua"))()
 end
 
 -- Make a Army ( need bypass not use)
 if p == 137969408767471 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Make%20a%20Army.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Make%20a%20Army.lua"))()
 end
 
 -- Break in (Story)
 if p == 4620170611 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Scripts/Break%20in%20Story.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Break%20in%20Story.lua"))()
+end
+
+-- Be a Beggar
+if p == 119574637420814 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-2025/refs/heads/main/Games/Break%20in%20Story.lua"))()
 end
